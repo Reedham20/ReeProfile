0ece513bad04fa7af69ae346e416bcb393fd73d9
Reedham Pujara <reedhamjpujara2092001@gmail.com>
2021-11-08 19:41:47 +0530

    trying hello on new branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-// import logo from './logo.svg';
-import './App.css';
-import SVG from './SVG';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>eedham Pujara</h1>
-        <SVG/>
-        <p>
-          Something amazing coming Soon
-        </p>
-        untill then visit my 
-        <a
-          className="App-link"
-          href="https://github.com/Reedham20"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub profile
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
